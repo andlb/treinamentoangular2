18a7e35b0a99ed8b8db036e576e7d3d447988cfe
andre luis <andlbp@gmail.com>
2017-07-27 14:49:26 -0300

    Iniciando desenvolvimento do servico

--- client/src/app/cadastro/empresa/empresaservico/empresaservico.component.ts
+++ client/src/app/cadastro/empresa/empresaservico/empresaservico.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { Servico } from './servico.model';
+import { EmpresaService } from './../empresa.service';
 
 @Component({
   selector: 'app-empresaservico',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empresaservico.component.css']
 })
 export class EmpresaservicoComponent implements OnInit {
+  @ViewChild('nome') nome: ElementRef;
+  form: FormGroup;
+  servico:Servico[]=[];
+  edit=false;
+  indexFuncionario=-1;
+  actinButtonValue = "Incluir"
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private empresaService: EmpresaService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onEditItem(index){
+  }
+  onDeletar(){
+    this.onLimpar();
+  }
+
+  onLimpar(){
+
   }
 
 }
---
+++ client/src/app/cadastro/empresa/empresaservico/servico.model.ts
@@ -0,0 +1,3 @@
+export class Servico {
+  constructor(public descricao: string, public quilometragem: number, public tempo: number) {}
+}
