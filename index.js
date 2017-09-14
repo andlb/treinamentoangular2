445369cbc62079bc9a89b27d79d001cf4eb3eb8a
andre luis <andlbp@gmail.com>
2017-09-14 20:10:12 -0300

    deletando os arquivos desnecessário

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(config.uri, (err) => {
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/client/dist/'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -39,7 +39,7 @@ app.use("/ordemservico", ordemservico);
 app.use("/proprietario", proprietario);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 const PORT = process.env.PORT || 8080
