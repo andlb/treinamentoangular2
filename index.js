f1f697ee8c6e996d6a73d114eb620ea2bd6f6de0
andre luis <andlbp@gmail.com>
2017-09-19 20:31:24 -0300

    deployment

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ mongoose.connect(config.uri,options, (err) => {
         }
     });
 });
-app.use(cors({ origin: 'http://localhost:4200' }));
+//app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
